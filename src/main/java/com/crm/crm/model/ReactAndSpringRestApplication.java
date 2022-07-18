678d27a0d5eb804b411b0a5ba1fcb3377bcb2d4e
ycoodr <ycoodr@gmail.com>
2022-07-18 13:45:51 +0300

    backend

---
+++ src/main/java/com/crm/crm/model/ReactAndSpringRestApplication.java
@@ -0,0 +1,12 @@
+package com.crm.crm.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactAndSpringRestApplication.class, args);
+    }
+
+}
